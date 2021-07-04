bb9bf5b37c80d94cc803a1560f80795125eb48c3
lampeduza <lmdogg@mail.ru>
2021-07-05 00:17:18 +0700

    Updates

--- js/slider2.js
+++ js/slider2.js
@@ -1,29 +1,29 @@
-// for index.html
-let sliders = document.querySelectorAll('.description__gallery');
+// for gallery.html
+let sliders = document.querySelectorAll('.photos__section');
 
 for (let i = 0; i < sliders.length; i++) {
   init_slider(sliders[i]);
 }
 
 function init_slider(slider) {
-  let slide = slider.querySelectorAll('.gallery__slider-item');
+  let slide = slider.querySelectorAll('.photos__item');
 
-  let next = slider.querySelector('.gallery__button--next');
-  let previous = slider.querySelector('.gallery__button--previous');
+  let next = slider.querySelector('.photos__button--next');
+  let previous = slider.querySelector('.photos__button--previous');
   let dots = slider.querySelectorAll(".rectangle-list__item");
 
   let i = 0;
 
   next.addEventListener('click', function () {
-    slide[i].classList.remove('gallery__slider-item--active');
+    slide[i].classList.remove('photos__item--active');
     dots[i].classList.remove('rectangle-list__item--active');
     i = (i + 1) % slide.length; // (*1)
-    slide[i].classList.add('gallery__slider-item--active');
+    slide[i].classList.add('photos__item--active');
     dots[i].classList.add('rectangle-list__item--active');
   });
 
   previous.addEventListener('click', function () {
-    slide[i].classList.remove('gallery__slider-item--active');
+    slide[i].classList.remove('photos__item--active');
     dots[i].classList.remove('rectangle-list__item--active');
     i = (i - 1) % slide.length; // (*1)
 
@@ -31,7 +31,7 @@ function init_slider(slider) {
       i = slide.length - 1;
     }
 
-    slide[i].classList.add('gallery__slider-item--active');
+    slide[i].classList.add('photos__item--active');
     dots[i].classList.add('rectangle-list__item--active');
   });
 }
